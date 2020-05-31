5db0a842adb88fac9d7c92c2d8a02dfe0acf4760
Setu <sid.auburn@gmail.com>
2020-05-31 17:16:42 -0500

    added upload function, setup requirements.txt file and setup test uploadfile

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, request, redirect, send_file
+import boto3
 
 app = Flask(__name__)
 
@@ -10,6 +11,27 @@ def entry_point():
 @app.route('/storage')
 def storage():
     return  render_template('index.html')
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    if request.method == "POST":
+        # f = request.files['file']
+        # f.save(os.path.join(UPLOAD_FOLDER, f.filename))
+        upload_file("upload-file/test-file.txt", "experiment-video-upload-bucket")
+
+        return redirect("/storage")
+
+
+def upload_file(file_name, bucket):
+    """
+    Function to upload a file to an S3 bucket
+    """
+    object_name = file_name
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_file(file_name, bucket, object_name)
+
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
