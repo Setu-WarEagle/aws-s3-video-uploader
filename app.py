204ce5df5b6b81170227d5b0372b8cd14284e264
“Pratik <patel.parth200@gmail.com>
2020-05-31 19:38:09 -0500

    creating a wrapper progress func

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect, send_file, Response
 import boto3
+import time
 
 app = Flask(__name__)
 
@@ -16,9 +17,16 @@ def storage():
 def upload():
     if request.method == "POST":
          f = request.files['file']
-         f.save(os.path.join('upload-file', f.filename))
-         upload_file(f"upload-file/{f.filename}", "experiment-video-upload-bucket")
-         return redirect("/storage")
+         Response(uploading_progress(f), mimetype='text/event-stream')
+
+def uploading_progress(f):
+    yield "Saving the file: {}. Please be patient...."
+    f.save(os.path.join('upload-file', f.filename))
+    yield "Saved! Starting to upload to S3 ..."
+    upload_file(f"upload-file/{f.filename}", "experiment-video-upload-bucket")
+    yield "Uploaded to S3! redirecting to home page now..."
+    time.sleep(5)
+    return redirect("/storage")
 
 
 def upload_file(file_name, bucket):
