975e7231bfe1653873cf70e1e6c81a4cb67099e2
“Pratik <patel.parth200@gmail.com>
2020-05-31 19:45:32 -0500

    creating a wrapper progress func

--- app.py
+++ app.py
@@ -15,19 +15,17 @@ def storage():
 
 @app.route("/upload", methods=['POST'])
 def upload():
-    if request.method == "POST":
-         f = request.files['file']
-         return Response(uploading_progress(f), mimetype='text/event-stream')
+    def uploading_progress(f):
+        yield "Saving the file: {}. Please be patient...."
+        f.save(os.path.join('upload-file', f.filename))
+        yield "Saved! Starting to upload to S3 ..."
+        upload_file(f"upload-file/{f.filename}", "experiment-video-upload-bucket")
+        yield "Uploaded to S3! redirecting to home page now..."
+        time.sleep(5)
+        return redirect("/storage")
 
-def uploading_progress(f):
-    yield "Saving the file: {}. Please be patient...."
-    f.save(os.path.join('upload-file', f.filename))
-    yield "Saved! Starting to upload to S3 ..."
-    upload_file(f"upload-file/{f.filename}", "experiment-video-upload-bucket")
-    yield "Uploaded to S3! redirecting to home page now..."
-    time.sleep(5)
-    return redirect("/storage")
-
+    f = request.files['file']
+    return Response(uploading_progress(f), mimetype='text/event-stream')
 
 def upload_file(file_name, bucket):
     """
